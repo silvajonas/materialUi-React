27411b0c0b0af67da451a8873a3b1206c1f6d053
silvajonas <ccastrojonas@outlook.com>
2022-12-11 23:00:01 -0300

    feat: Adicionado o react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-
+import {BrowserRouter} from 'react-router-dom'
+import { AppRoutes } from './routes'
 
 export const App = () => {
 
   return (
-    <div>
-      Jonas
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+    
   )
 }
